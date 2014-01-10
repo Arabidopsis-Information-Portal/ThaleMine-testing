d616dc873210923b1206bb0f666fc253ecef1196
Ntino Krampis <agbiotec@gmail.com>
2014-01-10 16:02:42 -0500

    scripts testing requests for specific genes

--- test_script.sh
+++ test_script.sh
@@ -2,7 +2,7 @@
 
 for i in {1..1000}
 do
-curl -sk "http://araport:araport!23@apps.araport.org/thalemine/begin.do" > "thalemine.$i"
+curl -sk "http://a****:a*****@apps.araport.org/thalemine/begin.do" > "thalemine.$i"
 SLEEP=$(( $RANDOM % 30 ))
 sleep ${SLEEP}
 done
